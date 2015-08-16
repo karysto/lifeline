5bcd9ada06cdad24d785735be42610115159e299
Thanasi Karachotzitis <thanasi.karachotzitis@mail.utoronto.ca>
2015-08-16 02:43:39 -0700

    Make counter count downwards.

--- src/popup.c
+++ src/popup.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static Window *s_pop_window;
-static TextLayer *s_output_pop_layer, *s_countdown_layer;
-static AppTimer *s_app_timer;
-static int counter = 0;
-static char buf[] = "1234567890";
+static Window    *s_pop_window;
+static TextLayer *s_output_pop_layer;
+static TextLayer *s_countdown_layer;
+static AppTimer  *s_app_timer;
+const  int  COUNTDOWN_SECONDS = 7;
+static int  counter           = 7;
+static char buf[]             = "1234567890";
 
 static void app_timer_callback(void *context){
    APP_LOG(APP_LOG_LEVEL_DEBUG, "timer callback"); 
-   if (counter < 7) {
-     counter++;
+   if (counter > 0) {
+     counter--;
 
      // convert the counter into a string
      snprintf(buf, sizeof(buf), "%d", counter);
      text_layer_set_text(s_countdown_layer, buf);
-
      s_app_timer = app_timer_register(1000, app_timer_callback, NULL);
    } 
 
@@ -34,13 +35,14 @@ static void pop_window_load(Window *window) {
   s_output_pop_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
   text_layer_set_font(s_output_pop_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_text(s_output_pop_layer, "Health State: Seizure");
+  text_layer_set_text_alignment(s_output_pop_layer, GTextAlignmentCenter);
   text_layer_set_overflow_mode(s_output_pop_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_pop_layer));
 
   // Create output TextLayer for countdown
   s_countdown_layer = text_layer_create(GRect(30, 50, 100, 50));
   text_layer_set_font(s_countdown_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  text_layer_set_text(s_countdown_layer, "0");
+  text_layer_set_text(s_countdown_layer, "7");
   text_layer_set_text_alignment(s_countdown_layer, GTextAlignmentCenter);
   text_layer_set_overflow_mode(s_countdown_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_countdown_layer));
@@ -64,7 +66,7 @@ static void pop_window_unload(Window *window) {
 }
 
 void pop_window_push(){
-    counter = 0; 
+    counter = COUNTDOWN_SECONDS; 
     s_pop_window = window_create();
     window_set_window_handlers(s_pop_window, (WindowHandlers) {
         .load = pop_window_load,
