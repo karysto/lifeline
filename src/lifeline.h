a0cd4583209401f558a2480582ec6d507ac761d8
Thanasi Karachotzitis <thanasi.karachotzitis@mail.utoronto.ca>
2015-08-16 03:08:35 -0700

    Add lifeline header file.

---
+++ src/lifeline.h
@@ -0,0 +1,7 @@
+#include <pebble.h>
+
+static void data_handler(AccelData *data, uint32_t num_samples);
+
+static void main_window_load(Window *window);
+
+static void main_window_unload(Window *window);
