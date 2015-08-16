fc6cf7824fda9bfcbf6a62f397041630fd938d5f
Thanasi Karachotzitis <thanasi.karachotzitis@mail.utoronto.ca>
2015-08-16 01:58:01 -0700

    Add lifeline main file.

---
+++ src/lifeline.c
@@ -0,0 +1,116 @@
+/*
+ * Constructs a Window housing an output TextLayer to show data from 
+ * either modes of operation of the accelerometer.
+ */
+
+#include <pebble.h>
+#include "popup.h"
+
+#define TAP_NOT_DATA true
+
+static int SEIZURE_Z_THRESHOLD = 1500;
+static Window *s_main_window;
+static TextLayer *s_output_layer;
+
+static void data_handler(AccelData *data, uint32_t num_samples) {
+  // Long lived buffer
+  static char s_buffer[128];
+
+  // Detected seizure-like hand motion
+  if (data[0].z > SEIZURE_Z_THRESHOLD) {
+    snprintf(s_buffer, sizeof(s_buffer), "something is wrong!");
+
+    // Initiate confirmation popup window
+    pop_window_push();  
+  }
+
+  else {
+    snprintf(s_buffer, sizeof(s_buffer), "you are okay");
+ }
+
+  //Show the data
+  text_layer_set_text(s_output_layer, s_buffer);
+}
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  switch (axis) {
+  case ACCEL_AXIS_X:
+    if (direction > 0) {
+      text_layer_set_text(s_output_layer, "X axis positive.");
+    } else {
+      text_layer_set_text(s_output_layer, "X axis negative.");
+    }
+    break;
+  case ACCEL_AXIS_Y:
+    if (direction > 0) {
+      text_layer_set_text(s_output_layer, "Y axis positive.");
+    } else {
+      text_layer_set_text(s_output_layer, "Y axis negative.");
+    }
+    break;
+  case ACCEL_AXIS_Z:
+    if (direction > 0) {
+      text_layer_set_text(s_output_layer, "Z axis positive.");
+    } else {
+      text_layer_set_text(s_output_layer, "Z axis negative.");
+    }
+    break;
+  }
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_output_layer, "No data yet.");
+  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(s_output_layer);
+}
+
+static void init() {
+  // Create main Window
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  window_stack_push(s_main_window, true);
+
+  // Use tap service? If not, use data service
+  if (!TAP_NOT_DATA) {
+    // Subscribe to the accelerometer tap service
+    accel_tap_service_subscribe(tap_handler);
+  } else {
+    // Subscribe to the accelerometer data service
+    int num_samples = 3;
+    accel_data_service_subscribe(num_samples, data_handler);
+
+    // Choose update rate
+    accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  }
+}
+
+static void deinit() {
+  // Destroy main Window
+  window_destroy(s_main_window);
+
+  if (TAP_NOT_DATA) {
+    accel_tap_service_unsubscribe();
+  } else {
+    accel_data_service_unsubscribe();
+  }
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
